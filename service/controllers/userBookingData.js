3edf66ca9cad4ce14065a35b2f097be167470290
vinayallampalli <vinayallampalli1998@gmail.com>
2022-10-14 12:22:51 +0530

    bookdata

--- service/controllers/userBookingData.js
+++ service/controllers/userBookingData.js
@@ -1,29 +1,12 @@
 const Booking = require('../models/userBooking')
-const crypto = require('crypto')
 
 exports.booking = async (req, res) => {
-    let OrderId;
-    crypto.randomInt(0, 10000, (err, n) => {
-      if (err) throw err;
-      OrderId = n.toString().padStart(4, "0");
-    //   req.body.OrderId = OrderId
-    
-      console.log(req.body.OrderId)
-    })
-  
     console.log("booking Api Triggered")
     try{
-        
         console.log(req.body)
-       const order = new Booking({
-        userDetails: req.body.userDetails,
-        Orderid:OrderId,
-        serviceList:req.body.serviceList,
-        Amount:req.body.Amount
-        })
-        console.log("--------->",order)
-        await order.save();
-        // await book.save();
+        const book = new Booking(req.body)
+        console.log("--------->",book)
+        await book.save();
         res.status(200).json({"message":"success"})
 
     }
@@ -33,4 +16,27 @@ exports.booking = async (req, res) => {
 
     }
 
+}
+
+exports.userBookData= async(req,res)=>{
+    try{
+        console.log("book Data Api Triggered")
+        let getData = await Booking.find({userID:req.params.userID})
+        console.log("______",getData)
+       
+          if (!getData) {
+            return res.status(500).json({ message: err.message });
+          }
+          
+          else {
+            res.status(200).json({ success: true, "message": "Success" ,getData});
+          }
+        }
+    
+      
+    catch(err){
+        console.log(err);
+      res.status(404).json({'message':"fail"})
+  
+    }
 }
